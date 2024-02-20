79156b1aede1b4cb6d6627eafc89777fa6011896
ivor <ivorst84@gmail.com>
2024-02-20 17:43:28 +0100

    WIP connecting to mongoDb with mongoose

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,94 +1,124 @@
-import React from 'react';
+import React from "react";
 import {
-    MDBValidation,
+  MDBValidation,
   MDBValidationItem,
   MDBContainer,
   MDBInput,
   MDBCheckbox,
   MDBBtn,
-  MDBIcon
-}
-from 'mdb-react-ui-kit';
+  MDBIcon,
+} from "mdb-react-ui-kit";
 import { useState } from "react";
+import io from "socket.io-client";
 
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//     const myColl = client.db("admin").collection("apf_users")
-//     const querry = myColl.find({"username": formValue.username, "password": formValue.password})
-//     console.log(querry)
-
-
-
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
+const socket = io("http://localhost:3002"); // Replace with your server URL
 
 function App() {
-    const [formValue, setFormValue] = useState({
-        username: '',
-        password: '',
-      });
+  const [formValue, setFormValue] = useState({
+    username: "",
+    password: "",
+  });
 
-    const handleSubmit = (e) => {
-      console.log(formValue)
-      
-    }
+  const handleSubmit = (e) => {
+    console.log(formValue);
+    e.preventDefault();
+    socket.emit("userLogin", formValue);
+  };
 
-    const onChange = (e) => {
-        setFormValue({ ...formValue, [e.target.name]: e.target.value });
-      };
+  const onChange = (e) => {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+  };
   return (
-    <MDBValidation >
+    <MDBValidation>
+      <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+        <MDBValidationItem>
+          <MDBInput
+            value={formValue.fname}
+            onChange={onChange}
+            required
+            wrapperClass="mb-4"
+            name="username"
+            label="username"
+            id="username"
+            type="text"
+          />
+        </MDBValidationItem>
 
-   
-    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
-    <MDBValidationItem >
-      <MDBInput value={formValue.fname} onChange={onChange} required wrapperClass='mb-4' name="username" label='username' id='username' type='text'/>
-    </MDBValidationItem>
+        <MDBValidationItem>
+          <MDBInput
+            value={formValue.fname}
+            onChange={onChange}
+            required
+            wrapperClass="mb-4"
+            name="password"
+            label="Password"
+            id="password"
+            type="password"
+          />
+        </MDBValidationItem>
 
-    <MDBValidationItem >
-      <MDBInput value={formValue.fname} onChange={onChange} required wrapperClass='mb-4' name="password" label='Password' id='password' type='password'/>
-    </MDBValidationItem>
-
-      <div className="d-flex justify-content-between mx-3 mb-4">
-        <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-        <a href="!#">Forgot password?</a>
-      </div>
-
-      <MDBBtn onClick={handleSubmit} className="mb-4">Sign in</MDBBtn>
-
-      <div className="text-center">
-        <p>Not a member? <a href="#!">Register</a></p>
-        <p>or sign up with:</p>
-
-        <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='facebook-f' size="sm"/>
-          </MDBBtn>
-
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='twitter' size="sm"/>
-          </MDBBtn>
-
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='google' size="sm"/>
-          </MDBBtn>
-
-          <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-            <MDBIcon fab icon='github' size="sm"/>
-          </MDBBtn>
-
+        <div className="d-flex justify-content-between mx-3 mb-4">
+          <MDBCheckbox
+            name="flexCheck"
+            value=""
+            id="flexCheckDefault"
+            label="Remember me"
+          />
+          <a href="!#">Forgot password?</a>
         </div>
-      </div>
 
-    </MDBContainer>
+        <MDBBtn onClick={handleSubmit} className="mb-4">
+          Sign in
+        </MDBBtn>
+
+        <div className="text-center">
+          <p>
+            Not a member? <a href="#!">Register</a>
+          </p>
+          <p>or sign up with:</p>
+
+          <div
+            className="d-flex justify-content-between mx-auto"
+            style={{ width: "40%" }}
+          >
+            <MDBBtn
+              tag="a"
+              color="none"
+              className="m-1"
+              style={{ color: "#1266f1" }}
+            >
+              <MDBIcon fab icon="facebook-f" size="sm" />
+            </MDBBtn>
+
+            <MDBBtn
+              tag="a"
+              color="none"
+              className="m-1"
+              style={{ color: "#1266f1" }}
+            >
+              <MDBIcon fab icon="twitter" size="sm" />
+            </MDBBtn>
+
+            <MDBBtn
+              tag="a"
+              color="none"
+              className="m-1"
+              style={{ color: "#1266f1" }}
+            >
+              <MDBIcon fab icon="google" size="sm" />
+            </MDBBtn>
+
+            <MDBBtn
+              tag="a"
+              color="none"
+              className="m-1"
+              style={{ color: "#1266f1" }}
+            >
+              <MDBIcon fab icon="github" size="sm" />
+            </MDBBtn>
+          </div>
+        </div>
+      </MDBContainer>
     </MDBValidation>
   );
 }
--- src/mongoDb-server.js
+++ src/mongoDb-server.js
@@ -1,22 +1,90 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const socketIO = require("socket.io");
+const http = require("http");
+const { ObjectId } = require("mongodb");
 
-const uri = "mongodb+srv://admin:admin@cluster0.ftbeo7u.mongodb.net/?retryWrites=true&w=majority";
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-async function run() {
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "http://localhost:3000", // Change to the correct port of your React app
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+const PORT = process.env.PORT || 3002;
+
+const uri =
+  "mongodb+srv://admin:admin@cluster0.ftbeo7u.mongodb.net/?retryWrites=true&w=majority/apf_users";
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("userLogin", (data) => {
+    // console.log(data);
+    run(data).catch(console.dir);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+
+  socket.on("newUser", async (data) => {
+    try {
+      // Handle the new user data here (e.g., save to MongoDB)
+      console.log("New user data:", data);
+      // You can emit events back to the client if needed
+      // socket.emit("someEvent", someData);
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle error
+    }
+  });
+});
+
+const userSchema = new mongoose.Schema({
+  id: ObjectId,
+  name: String,
+  password: String,
+});
+
+const UserModel = mongoose.model("users", userSchema, "users");
+
+async function run(data) {
   try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
     await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await mongoose.connection.db("apf_users");
+    console.log("Connected to MongoDB!");
+    // const User = connection.model("user", userSchema);
+    console.log(UserModel.collection.collectionName);
+
+    console.log(data);
+    const result = await UserModel.findOne({
+      username: data.username,
+      password: data.password,
+    }).catch((error) => {
+      console.error("Error executing query:", error);
+    });
+    console.log(await UserModel.find());
+
+    // const result = await mongoose.connection.db
+    //   .collection("apf_users")
+    //   .find({ username: data.username, password: data.password });
+
+    console.log(result);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
   } finally {
     // Ensures that the client will close when you finish/error
     await mongoose.disconnect();
   }
 }
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // run().catch(console.dir);
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
